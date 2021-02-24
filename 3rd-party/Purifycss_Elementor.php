269efd991b9b8f6b1970db42a7f1493dd42634a0
Peter Bagi <peter.bagi@gmail.com>
2021-02-24 14:46:42 +0100

    remove call of undefined function

--- 3rd-party/Purifycss_Elementor.php
+++ 3rd-party/Purifycss_Elementor.php
@@ -5,8 +5,6 @@ class Purifycss_Elementor extends Purifycss_ThirdPartyExtension {
 
     public function run() {
         if (!PurifycssHelper::is_enabled()) return;
-
-        add_action( 'elementor/frontend/after_enqueue_styles', array($this->public, 'replace_all_styles'), PHP_INT_MAX );
     }
 
 }
